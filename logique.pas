cae41392ad2040c0f1f1217d7459ac0a9a5bc14c
Baptiste <bapt.hersent@gmail.com>
2022-10-31 22:00:15 +0100

    ajout du debut de la fonction checkNeighbors

--- logique.pas
+++ logique.pas
@@ -9,8 +9,11 @@ Const
 
 var grid : array[0..99] of Cell; // 100 cellules dans le labyrinthe
 var current : Cell; // variable contenant la cellule actuelle 
+var index : Integer;
 
 procedure show(grid : Array of Cell; screen : PSDL_Surface);
+procedure calcIndex(i, j : Integer);
+procedure checkNeighbors(cell : Cell);
 procedure setup;
 
 implementation
@@ -24,14 +27,42 @@ begin
         x := grid[count].i * w;
         y := grid[count].j * w;
         //grid[count].visited := true;
-        if grid[count].visited = true then fillRect(x, y, w, w, 255, 0, 255, screen);
+        if grid[count].visited = true then fillRect(x, y, w, w, 0, 0, 255, screen);
         if grid[count].walls[0] then fillRect(x        , y        , w, 1, 255, 255, 255, screen); // ligne à gauche de la cellule
         if grid[count].walls[1] then fillRect(x        , y        , 1, w, 255, 255, 255, screen); // ligne en haut de la cellule
         if grid[count].walls[2] then fillRect(x + w - 1, y        , 1, w, 255, 255, 255, screen); // ligne à droite de la cellule
         if grid[count].walls[3] then fillRect(x        , y + w - 1, w, 1, 255, 255, 255, screen); // ligne en bas de la cellule
         SDL_Flip(screen);
+        //writeln(grid[count].walls[0], grid[count].walls[3]);
     end;
+    checkNeighbors(grid[count]);
     
+end;
+
+procedure calcIndex(i, j : Integer);
+begin
+    index := i + j * (LARGEUR div w);
+end;
+
+procedure checkNeighbors(cell : Cell);
+var neighbors : Array[0..3] of Cell;
+var top, right, bottom, left : Cell;
+begin
+    calcIndex(cell.i, cell.j - 1);
+    top := grid[index];
+    calcIndex(cell.i + 1, cell.j);
+    right := grid[index];                   { ~~~~~~~~~ CF video part 2 a 9min ~~~~~~~~ }
+    calcIndex(cell.i, cell.j + 1);
+    bottom := grid[index];
+    calcIndex(cell.i - 1, cell.j);
+    left := grid[index];
+    //write(index); 
+
+    if top.visited <> true then neighbors[0] := top;
+    if right.visited <> true then neighbors[0] := right;
+    if bottom.visited <> true then neighbors[0] := bottom;
+    if left.visited <> true then neighbors[0] := left;
+
 end;
 
 procedure setup;
@@ -49,15 +80,19 @@ begin
             cells.i := i; // creer les cellules
             cells.j := j;
             cells.visited := false; // initialement les cellules n'ont pas ete visite
+            cells.walls[0] := true; // initialement les cellules ont les 4 murs
+            cells.walls[1] := true;
+            cells.walls[2] := true;
+            cells.walls[3] := true;
             //writeln(count,' ',cells.i, ' ',cells.j);
             grid[count] := cells; // ajouter les cellules dans un tableau
             count := count + 1;
         end;
     end;
     grid[0].visited := true;
-    grid[26].visited := true;
-    grid[48].visited := true;
-    grid[73].visited := true;
+    //grid[26].visited := true;
+    //grid[48].visited := true;
+    //grid[73].visited := true;
 end;
 
 end.
